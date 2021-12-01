187616242de741df5fe8562fa07f3347042c3ad4
zuishuainanhai <1705902650@qq.com>
2021-12-01 21:02:57 +0800

    vue3组件化api结合js原生实现

--- src/views/Layout/components/header-sticky.vue
+++ src/views/Layout/components/header-sticky.vue
@@ -13,38 +13,47 @@
 
 <script>
 import HeaderNav from './header-nav'
-// import { ref, onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 export default {
   name: 'AppHeaderSticky',
   components: { HeaderNav },
-  data () {
-    return {
-      isShow: false
-    }
-  },
+  // data () {
+  //   return {
+  //     isShow: false
+  //   }
+  // },
   // 原生js
-  created () {
-    window.onscroll = () => {
-      // 滚动高度
-      const hights = document.documentElement.scrollTop
-      if (hights > 100) {
-        this.isShow = true
-      } else {
-        this.isShow = false
+  // created () {
+  //   window.onscroll = () => {
+  //     // 滚动高度
+  //     const hights = document.documentElement.scrollTop
+  //     if (hights > 100) {
+  //       this.isShow = true
+  //     } else {
+  //       this.isShow = false
+  //     }
+  //   }
+  // },
+  setup () {
+    // 定义一个变量存储是否可见吸顶组件
+    // 默认不显示
+    const isShow = ref(false)
+    // 实现滚动事件显示吸顶组件  -----
+    // 实际就是window的滚动事件拿到滚动高度当满足条件是就去控制
+    onMounted(() => {
+      window.onscroll = () => {
+        // 滚动高度
+        const hights = document.documentElement.scrollTop
+        // console.log('页面的滚动高度', hights)
+        if (hights > 100) {
+          isShow.value = true
+        } else {
+          isShow.value = false
+        }
       }
-    }
+    })
+    return { isShow }
   }
-  // setup () {
-  //   // 定义一个变量存储是否可见吸顶组件
-  //   // 默认不显示
-  //   const isShow = ref(false)
-  //   // 实现滚动事件显示吸顶组件  -----
-  //   // 实际就是window的滚动事件拿到滚动高度当满足条件是就去控制
-  //   onMounted(() => {
-  //     window.onscroll = () => { }
-  //   })
-  //   return { isShow }
-  // }
 }
 </script>
 
