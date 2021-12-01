b0d3420f2c82c00e7df75c04a07bca40cdaa23a3
zuishuainanhai <1705902650@qq.com>
2021-12-01 13:56:19 +0800

    使用vue-cli的style-resoures-loader插件自动注入到每个vue组件中style标签中

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -12,7 +12,7 @@ export default {
 
 <style lang="less" scoped>
 // 手动引入less变量样式
-@import "~@/styles/variables.less";
+// @import "~@/styles/variables.less";
 .box {
   color: @xtxColor;
 }
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,22 @@
+const path = require('path')
 module.exports = {
   lintOnSave: true,
+
   // 打包后可以部署到任意目录
   // publicPath: './',
   devServer: {
     open: true
+  },
+
+  pluginOptions: {
+    // 记得重启服务器
+    'style-resources-loader': {
+      // 自动注入less文件到页面的style中
+      preProcessor: 'less',
+      // 配置哪些文件需要自动导入
+      patterns: [
+        path.join(__dirname, '/src/styles/variables.less')
+      ]
+    }
   }
 }
