26e1817b8bac71d52f2658eb130bdfa29288bfad
zuishuainanhai <1705902650@qq.com>
2021-12-01 14:12:16 +0800

    重置样式与公用样式

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 重置浏览器默认样式
+import 'normalize.css'
+// 全局公共样式
+import '@/styles/common.less'
 createApp(App).use(store).use(router).mount('#app')
