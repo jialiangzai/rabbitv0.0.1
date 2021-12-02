875767cb07b47969018ba5c830bdd2e704ee2259
zuishuainanhai <1705902650@qq.com>
2021-12-02 15:34:55 +0800

    使用封装好的面板组件实现新鲜好物数据渲染显示

--- src/api/home.js
+++ src/api/home.js
@@ -16,3 +16,11 @@ export const findHeadCategory = () => {
 export const findBanner = () => {
   return request('home/banner', 'get')
 }
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNew = () => {
+  return request('home/new', 'get')
+}
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -5,21 +5,24 @@
     <!-- 右侧轮播图 -->
     <HomeBanner />
     <!-- 测试商品电梯导航 -->
-    <HomePanel title="主标题" subTitle="副标题">
-      <template #right> </template>
-      <!-- 在模板上使用插槽和vue2类似 -->
-      <h2>我是内容</h2>
-    </HomePanel>
+    <!-- <HomePanel title="主标题" subTitle="副标题"> -->
+    <!-- <template #right> </template> -->
+    <!-- 在模板上使用插槽和vue2类似 -->
+    <!-- <h2>我是内容</h2> -->
+    <!-- </HomePanel> -->
+    <!-- 商品电梯导航 -->
+    <HomeNew> </HomeNew>
   </div>
 </template>
 
 <script>
 import HomeCategory from './components/home-category'
 import HomeBanner from './components/home-banner'
-import HomePanel from './components/home-panel.vue'
+// import HomePanel from './components/home-panel.vue'
+import HomeNew from './components/home-new.vue'
 export default {
   components: {
-    HomeCategory, HomeBanner, HomePanel
+    HomeCategory, HomeBanner, HomeNew
   },
   setup () {
     return {}
