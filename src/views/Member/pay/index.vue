b1c7296875d79ee481c44f6fc51fca8ccf35410b
zuishuainanhai <1705902650@qq.com>
2021-12-08 21:05:44 +0800

    准备支付

--- src/views/Member/pay/index.vue
+++ src/views/Member/pay/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="xtx-pay-page">
     <div class="container">
+      <!-- 面包屑 -->
       <XtxBread>
         <XtxBreadItem to="/">首页</XtxBreadItem>
         <XtxBreadItem to="/cart">购物车</XtxBreadItem>
@@ -9,13 +10,20 @@
       <!-- 付款信息 -->
       <div class="pay-info">
         <span class="icon iconfont icon-queren2"></span>
-        <div class="tip">
+        <!-- orderState - 订单状态，1为待付款、2为待发货(已付款)、3为待收货、4为待评价、5为已完成、6为已取消或超时 -->
+        <div class="tip" v-if="order.orderState === 1">
           <p>订单提交成功！请尽快完成支付。</p>
           <p>支付还剩 <span>24分59秒</span>, 超时后将取消订单</p>
         </div>
+        <div class="tip" v-if="order.orderState === 2">
+          <p>订单已支付！</p>
+        </div>
+        <div class="tip" v-if="order.orderState === 6">
+          <p>订单支付超时！</p>
+        </div>
         <div class="amount">
           <span>应付总额：</span>
-          <span>¥5673.00</span>
+          <span>¥{{ order.payMoney }}</span>
         </div>
       </div>
       <!-- 付款方式 -->
@@ -39,8 +47,23 @@
   </div>
 </template>
 <script>
+import { ref } from 'vue'
+import { findOrder } from '@/api/order'
+import { useRoute } from 'vue-router'
 export default {
-  name: 'XtxPayPage'
+  name: 'XtxPayPage',
+  setup () {
+    const order = ref({})
+    const route = useRoute()
+    const getOrder = async () => {
+      // 做订单倒计时----有效期处理 释放库存(名额)
+      const { result } = await findOrder(route.query.id)
+      console.log('订单数据之支付', result)
+      order.value = result
+    }
+    getOrder()
+    return { order, getOrder }
+  }
 }
 </script>
 <style scoped lang="less">
@@ -108,10 +131,12 @@ export default {
       border-color: @xtxColor;
     }
     &.alipay {
-      background: url(https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/7b6b02396368c9314528c0bbd85a2e06.png) no-repeat center / contain;
+      background: url(https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/7b6b02396368c9314528c0bbd85a2e06.png)
+        no-repeat center / contain;
     }
     &.wx {
-      background: url(https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/c66f98cff8649bd5ba722c2e8067c6ca.jpg) no-repeat center / contain;
+      background: url(https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/c66f98cff8649bd5ba722c2e8067c6ca.jpg)
+        no-repeat center / contain;
     }
   }
 }
